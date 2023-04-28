4b3fd150de30f063ffa7bbf5b08f4f656fc91292
EmmanuelJYokoyama <emmanuelnoronha2@gmail.com>
2023-04-27 21:33:20 -0300

    UPDATE - 27/04 - feito todas as funçoes

--- app/src/main/java/com/example/manonaosei/MainActivity.kt
+++ app/src/main/java/com/example/manonaosei/MainActivity.kt
@@ -3,58 +3,121 @@ package com.example.manonaosei
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.FirebaseDatabase
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private var TAG: String = "APP"
     private lateinit var email: EditText
     private lateinit var senha : EditText
+    private lateinit var btn_reset: Button
+    private lateinit var btn_login : EditText
+    private lateinit var btn_delete : EditText
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         auth = FirebaseAuth.getInstance()
-        var btn: Button = findViewById(R.id.btn)
+        btn_reset = findViewById(R.id.btn_Reset)
+        btn_login = findViewById(R.id.btn_Log)
+        btn_delete = findViewById(R.id.btn_Del)
         email = findViewById(R.id.email)
         senha = findViewById(R.id.password)
 
+        login_cadastro()
+        resetPass()
+        deleteUser()
 
 
     }
 
-    fun VerificarUsuario(email:String, password:String) {
-        auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    Toast.makeText(baseContext, "Logado com sucesso.", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(baseContext, "Erro Login.", Toast.LENGTH_SHORT).show()
-                }
 
-            }
-    }
 
-    fun UserCreate(email: String, pass: String){
-        var text: TextView = findViewById(R.id.txtvw);
-
-        auth.createUserWithEmailAndPassword(email, pass)
+    fun Usuario_create(email:String, senha:String){
+        auth.createUserWithEmailAndPassword(email, senha)
             .addOnCompleteListener(this){ task->
                 if(task.isSuccessful){
                     var usuario :FirebaseUser? = auth.currentUser
                     var idUsuario: String = usuario!!.uid
-                    text.text = "Usuario Criado"
+                    Toast.makeText(baseContext, "Usuario Criado!", Toast.LENGTH_LONG).show()
+
                 }else{
-                    text.text = "Falha"
+                    Toast.makeText(baseContext, "Falha na criação!", Toast.LENGTH_LONG).show()
+
                 }
             }
+
+    }
+
+    fun login_cadastro(){
+        btn_login.setOnClickListener{
+            var email: String = email.text.toString()
+            var senha: String = senha.text.toString()
+
+            verifyUser(email, senha)
+
+        }
+
+    }
+
+    fun resetPass(){
+        btn_reset.setOnClickListener {
+            var senha: String = senha.text.toString()
+            val usuario = FirebaseAuth.getInstance().currentUser
+            usuario!!.updatePassword(senha).addOnCompleteListener{task ->
+                if(task.isSuccessful){
+                    Toast.makeText(baseContext, "Password reseted sucessfully",Toast.LENGTH_LONG).show()
+
+                }else{
+                    Toast.makeText(baseContext, "/-/-/-/-",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    fun deleteUser(){
+        val usuario = FirebaseAuth.getInstance().currentUser
+        btn_delete.setOnClickListener {
+            usuario!!.delete().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext, "Delete Sucessful", Toast.LENGTH_LONG).show()
+
+                }
+            }
+        }
+    }
+
+
+    fun verifyUser(email: String, pass: String){
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this){ task ->
+            if(task.isSuccessful){
+                val usuario = auth.currentUser
+                if(usuario != null){
+                    if(usuario.isEmailVerified){
+                        usuario.sendEmailVerification().addOnCompleteListener{task ->
+                            if(task.isSuccessful){
+                                Log.d(TAG, "Email sent!")
+                            }
+                        }
+                    }
+                }
+                Toast.makeText(baseContext, "Login Sucessful",Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(baseContext, "Login error, sign up in process.",Toast.LENGTH_LONG).show()
+                Usuario_create(email,pass)
+            }
+
+        }
+
+
     }
 }
